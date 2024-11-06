a85f21729c52b5f9b2b2941a65c492b489470c66
Simen Abelsen <simen.nordbo.abelsen@brilliant.no>
2024-11-06 22:56:20 +0100

    ✨ Create first functioning game :D

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use bevy::app::AppExit;
+use bevy::audio::*;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use rand::prelude::*;
@@ -6,25 +8,74 @@ use rand::prelude::*;
 pub const PLAYER_SIZE: f32 = 64.0;
 pub const PLAYER_SPEED: f32 = 500.0;
 // Enemies
-pub const NUMBER_OF_ENEMIES: usize = 10;
+pub const NUMBER_OF_ENEMIES: usize = 5;
 pub const ENEMY_SPEED: f32 = 200.0;
 pub const ENEMY_SIZE: f32 = 64.0;
+pub const ENEMY_SPAWN_TIME: f32 = 5.0;
+// Misc
+pub const NUMBER_OF_STARS: usize = 10;
+pub const STAR_SIZE: f32 = 30.0;
+pub const STAR_SPAWN_TIME: f32 = 1.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_player, spawn_enemies, spawn_camera))
+        .init_resource::<SoundEffectHandles>()
+        .init_resource::<Score>()
+        .init_resource::<HighScores>()
+        .init_resource::<StarSpawnTimer>()
+        .init_resource::<EnemySpawnTimer>()
+        .add_event::<GameOver>()
+        .add_systems(
+            Startup,
+            (spawn_player, spawn_enemies, spawn_camera, spawn_stars),
+        )
+        .add_systems(Update, (player_movement, confine_player_movement))
         .add_systems(
             Update,
             (
-                player_movement,
-                confine_player_movement,
                 enemy_movement,
                 update_enemy_direction,
                 confine_enemy_movement,
+                enemy_hit_player,
+                tick_enemy_spawn_timer,
+                spawn_enemy,
+            ),
+        )
+        .add_systems(
+            Update,
+            (player_hit_star, update_score, tick_spawn_stars, spawn_star),
+        )
+        .add_systems(
+            Update,
+            (
+                exit_game,
+                handle_game_over,
+                update_high_scores,
+                high_scores_updated,
             ),
         )
         .run();
+}
+
+#[derive(Resource)]
+pub struct SoundEffectHandles {
+    pluck1: Handle<AudioSource>,
+    pluck2: Handle<AudioSource>,
+    explosion: Handle<AudioSource>,
+    laser_large: Handle<AudioSource>,
+}
+
+impl FromWorld for SoundEffectHandles {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.resource::<AssetServer>();
+        Self {
+            pluck1: asset_server.load("audio/pluck_001.ogg"),
+            pluck2: asset_server.load("audio/pluck_002.ogg"),
+            explosion: asset_server.load("audio/explosionCrunch_000.ogg"),
+            laser_large: asset_server.load("audio/laserLarge_000.ogg"),
+        }
+    }
 }
 
 #[derive(Component)]
@@ -33,6 +84,68 @@ pub struct Player {}
 #[derive(Component)]
 pub struct Enemy {
     pub direction: Vec2,
+}
+
+#[derive(Resource)]
+pub struct Score {
+    pub value: u32,
+}
+
+impl Default for Score {
+    fn default() -> Self {
+        Self { value: 0 }
+    }
+}
+
+#[derive(Resource, Debug)]
+pub struct HighScores {
+    pub scores: Vec<(String, u32)>,
+}
+
+impl Default for HighScores {
+    fn default() -> Self {
+        Self { scores: Vec::new() }
+    }
+}
+
+#[derive(Resource)]
+pub struct StarSpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for StarSpawnTimer {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(STAR_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct EnemySpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for EnemySpawnTimer {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(ENEMY_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct Star {}
+
+#[derive(Event)]
+pub struct GameOver {
+    pub score: u32,
+}
+
+impl Default for GameOver {
+    fn default() -> Self {
+        Self { score: 0 }
+    }
 }
 
 pub fn spawn_player(
@@ -86,6 +199,29 @@ pub fn spawn_enemies(
         ));
     }
 }
+
+pub fn spawn_stars(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..NUMBER_OF_STARS {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/star.png"),
+                ..default()
+            },
+            Star {},
+        ));
+    }
+}
+
 pub fn player_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut player_query: Query<&mut Transform, With<Player>>,
@@ -154,6 +290,8 @@ pub fn enemy_movement(mut enemy_query: Query<(&mut Transform, &mut Enemy)>, time
 pub fn update_enemy_direction(
     mut enemy_query: Query<(&Transform, &mut Enemy)>,
     window_query: Query<&Window, With<PrimaryWindow>>,
+    // sound_effects: Res<SoundEffectHandles>,
+    // mut commands: Commands,
 ) {
     let window = window_query.get_single().unwrap();
 
@@ -165,13 +303,28 @@ pub fn update_enemy_direction(
 
     for (transform, mut enemy) in enemy_query.iter_mut() {
         let translation = transform.translation;
+        // let mut direction_changed = false;
         if translation.x < x_min || translation.x > x_max {
             enemy.direction.x *= -1.0;
+            // direction_changed = true;
         }
 
         if translation.y < y_min || translation.y > y_max {
             enemy.direction.y *= -1.0;
+            // direction_changed = true;
         }
+
+        // if direction_changed {
+        //     let sound = if random::<f32>() > 0.5 {
+        //         sound_effects.pluck1.clone()
+        //     } else {
+        //         sound_effects.pluck2.clone()
+        //     };
+        //     commands.spawn(AudioBundle {
+        //         source: sound,
+        //         settings: PlaybackSettings::DESPAWN,
+        //     });
+        // }
     }
 }
 
@@ -202,5 +355,151 @@ pub fn confine_enemy_movement(
         }
 
         transform.translation = translation;
+    }
+}
+
+pub fn enemy_hit_player(
+    mut commands: Commands,
+    mut event_writer: EventWriter<GameOver>,
+    mut player_query: Query<(Entity, &Transform), With<Player>>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+    score: Res<Score>,
+    sound_effects: Res<SoundEffectHandles>,
+) {
+    if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
+        for enemy_transform in enemy_query.iter() {
+            let distance = player_transform
+                .translation
+                .distance(enemy_transform.translation);
+            let player_radius = PLAYER_SIZE / 2.0;
+            let enemy_radius = ENEMY_SIZE / 2.0;
+            if distance < player_radius + enemy_radius {
+                println!("Enemy hit player! Game over!");
+                commands.spawn(AudioBundle {
+                    source: sound_effects.explosion.clone(),
+                    settings: PlaybackSettings::DESPAWN,
+                });
+                commands.entity(player_entity).despawn();
+                event_writer.send(GameOver { score: score.value });
+            }
+        }
+    }
+}
+
+pub fn player_hit_star(
+    mut commands: Commands,
+    player_query: Query<&Transform, With<Player>>,
+    star_query: Query<(Entity, &Transform), With<Star>>,
+    sound_effects: Res<SoundEffectHandles>,
+    mut score: ResMut<Score>,
+) {
+    if let Ok(player_transform) = player_query.get_single() {
+        for (star_entity, star_transform) in star_query.iter() {
+            let distance = player_transform
+                .translation
+                .distance(star_transform.translation);
+            let player_radius = PLAYER_SIZE / 2.0;
+            let star_radius = STAR_SIZE / 2.0;
+            if distance < player_radius + star_radius {
+                println!("Player hit star!");
+                score.value += 1;
+                commands.spawn(AudioBundle {
+                    source: sound_effects.laser_large.clone(),
+                    settings: PlaybackSettings::DESPAWN,
+                });
+                commands.entity(star_entity).despawn();
+            }
+        }
+    }
+}
+
+pub fn update_score(score: Res<Score>) {
+    if score.is_changed() {
+        println!("Score: {}", score.value.to_string());
+    }
+}
+
+pub fn tick_spawn_stars(mut star_spawn_timer: ResMut<StarSpawnTimer>, time: Res<Time>) {
+    star_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_star(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    star_spawn_timer: Res<StarSpawnTimer>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if star_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/star.png"),
+                ..default()
+            },
+            Star {},
+        ));
+    }
+}
+
+pub fn tick_enemy_spawn_timer(mut enemy_spawn_timer: ResMut<EnemySpawnTimer>, time: Res<Time>) {
+    enemy_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_enemy(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    enemy_spawn_timer: Res<EnemySpawnTimer>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if enemy_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(),
+            },
+        ));
+    }
+}
+
+pub fn exit_game(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut app_exit_events: EventWriter<AppExit>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        app_exit_events.send(AppExit::Success);
+    }
+}
+
+pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+    for event in game_over_event_reader.read() {
+        println!("Game over! Score: {}", event.score);
+    }
+}
+
+pub fn update_high_scores(
+    mut high_scores: ResMut<HighScores>,
+    mut event_reader: EventReader<GameOver>,
+) {
+    for event in event_reader.read() {
+        high_scores.scores.push(("Player".to_string(), event.score));
+    }
+}
+
+pub fn high_scores_updated(high_scores: Res<HighScores>) {
+    if high_scores.is_changed() {
+        println!("High scores: {:?}", high_scores);
     }
 }
